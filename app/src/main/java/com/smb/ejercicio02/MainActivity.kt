e68585a375bdbf78f9d77fa8870157c558b16ed3
Salvador <salvador.martos@gmail.com>
2023-09-28 12:45:05 +0200

    Hasta MyStateExample

--- app/src/main/java/com/smb/ejercicio02/MainActivity.kt
+++ app/src/main/java/com/smb/ejercicio02/MainActivity.kt
@@ -3,16 +3,32 @@ package com.smb.ejercicio02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.smb.ejercicio02.ui.theme.Ejercicio02Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyBox()
+                    MyStateExample()
                 }
             }
         }
@@ -40,17 +56,149 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true, device = Devices.NEXUS_5)
+@Preview(
+    showBackground = true,
+    showSystemUi = false,
+    device = Devices.NEXUS_5,
+    fontScale = 1.2f
+)
 @Composable
 fun GreetingPreview() {
     Ejercicio02Theme {
-        MyBox()
+        MyStateExample()
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    var counter = 0
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado $counter veces")
+    }
+}
+
+@Composable
+fun MySpacer(size: Int) {
+    Spacer(modifier = Modifier.height(size.dp))
+}
+
+@Composable
+fun MyComplexLayout() {
+    Column(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Cyan)
+        ) {
+        }
+        MySpacer(size = 20)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Caja Roja",
+                    color = Color.White,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Green),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Caja Verde")
+            }
+        }
+
+        MySpacer(size = 20)
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Magenta)
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        repeat(14) {
+            Text(
+                text = "Ejemplo 1",
+                modifier = Modifier
+                    .width(150.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center
+    ) {
+        repeat(4) {
+            Text(
+                text = "Ejemplo 1",
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxWidth()
+                    .height(50.dp)
+            )
+
+        }
     }
 }
 
 @Composable
 fun MyBox() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Esto es un ejemplo")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .width(250.dp)
+                .height(250.dp)
+                .background(Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Esto es un ejemplo")
+        }
+
     }
 }
